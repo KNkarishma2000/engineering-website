4e62409ccb8aa340e0b88024e63e9466025ec08f
Karishma Khatri <114894267+KNkarishma2000@users.noreply.github.com>
2024-03-11 11:54:06 +0530

    Add files via upload

---
+++ engineering2/script.js
@@ -0,0 +1,11 @@
+// Initialize Slick Carousel
+$(document).ready(function(){
+    $('.slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>',
+    });
+});
